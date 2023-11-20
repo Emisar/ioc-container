e6e0c879aa768a5cce4d29a60066f01a29e2d55b
Emisar <BeadsmanJudge@gmail.com>
2023-11-20 17:31:19 +0500

    Remove unused import

--- src/main/java/emisar/soft/ioc/container/entity/FactoryBean.java
+++ src/main/java/emisar/soft/ioc/container/entity/FactoryBean.java
@@ -4,8 +4,6 @@ import emisar.soft.ioc.container.exception.BeanCreationException;
 
 import java.lang.reflect.Executable;
 import java.lang.reflect.Parameter;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
 
 public interface FactoryBean {
 
